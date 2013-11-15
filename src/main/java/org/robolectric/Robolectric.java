4ffe654167801d1c192593b5ee4e8780f01a3113
Roman Mazur <rmazur@stanfy.com.ua>
2013-11-15 20:50:12 +0200

    organize global state and fix reset

--- src/main/java/org/robolectric/Robolectric.java
+++ src/main/java/org/robolectric/Robolectric.java
@@ -268,6 +268,7 @@ import org.robolectric.shadows.ShadowResolveInfo;
 import org.robolectric.shadows.ShadowResourceCursorAdapter;
 import org.robolectric.shadows.ShadowResources;
 import org.robolectric.shadows.ShadowResultReceiver;
+import org.robolectric.shadows.ShadowSQLiteConnection;
 import org.robolectric.shadows.ShadowScaleGestureDetector;
 import org.robolectric.shadows.ShadowScanResult;
 import org.robolectric.shadows.ShadowScrollView;
@@ -1303,6 +1304,7 @@ public class Robolectric {
     ShadowStatFs.reset();
     ShadowTypeface.reset();
     ShadowCursorWindow.reset();
+    //ShadowSQLiteConnection.reset();
   }
 
   public static <T extends Activity> ActivityController<T> buildActivity(Class<T> activityClass) {
